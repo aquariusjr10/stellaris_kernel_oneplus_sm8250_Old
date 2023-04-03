c1a7157ea55f65c8c9279d25ad22e39c920d318c
dawfuk <dawfukfr@gmail.com>
2023-04-03 20:56:53 +0200

    refactor(arm64): disable SECCOMP in kona-perf_defconfig refactor(core_hook.c): remove unused seccomp fields from current process

--- drivers/kernelsu/core_hook.c
+++ drivers/kernelsu/core_hook.c
@@ -73,8 +73,6 @@ void escape_to_root(void)
 #else
 	current_thread_info()->flags &= ~(TIF_SECCOMP | _TIF_SECCOMP);
 #endif
-	current->seccomp.mode = 0;
-	current->seccomp.filter = NULL;
 
 	// setgroup to root
 	if (cred->group_info)
